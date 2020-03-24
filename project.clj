75d80ce4f431c5c34f1958eea15552c41dfab1d4
Lucas Severo <lucas.severo@captalys.com.br>
2020-03-24 13:37:07 -0300

    upgraded the JNA version, incremented the project version

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clj-xlsxio "0.5.0"
+(defproject clj-xlsxio "0.5.1"
   :description "xlsxio for clojure"
   :url "https://github.com/lsevero/clj-xlsxio"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[net.java.dev.jna/jna "5.4.0"]
+  :dependencies [[net.java.dev.jna/jna "5.5.0"]
                  [org.clojure/clojure "1.10.0"]
                  [joda-time "1.6"]]
   :profiles {:dev {:plugins [[cider/cider-nrepl "0.22.3"]
